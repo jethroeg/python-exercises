a93391156da7faa85067efb4c673b5dba8c33937
jethroeg <troygatbunton@gmail.com>
2022-06-26 10:03:07 +0800

    update

---
+++ 5.py
@@ -0,0 +1,13 @@
+# create a while loop that functions the same as this for loop 
+
+#furniture = ['table', 'chair', 'desk', 'lamp', 'couch']
+
+#for item in furniture:
+#    print(f'YOU WILL NOT BE ABLE TO FIND A {item.upper()} CHEAPER THAN OUR PRICES AT FURNITURE MART!'
+
+furniture = ['table', 'chair', 'desk', 'lamp', 'couch']
+i = 0
+
+while i < len(furniture):
+    print(f'YOU WILL NOT BE ABLE TO FIND A {furniture[i].upper()} CHEAPER THAN OUR PRICES AT FURNITURE MART!')
+    i += 1
